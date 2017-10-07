2857cea37449a006820607ac797492e1e8c65ccc
Daniele De Francesco <daniele@de-francesco.it>
2017-10-06 21:05:48 -0400

    maybe these files could be helpful

---
+++ spec/RewardsSpec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import '../public/index.html'
+
+describe('TaskList', function () {
+
+
+
+    it ('Should have a 30 when you type 30', function () {
+        true;
+    })
+})
