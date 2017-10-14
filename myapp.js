c50aba19c558a0c7ef7ee65ccf8a33fae427fc2a
Daniele De Francesco <daniele@de-francesco.it>
2017-10-14 18:50:40 -0400

    now listens and redirects

--- myapp.js
+++ myapp.js
@@ -20,9 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', express.static('public', {
+  index: 'todomvc.html'
+}));
+
+//app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -44,3 +47,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(80);
